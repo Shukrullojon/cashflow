083db9927d53711bbca8926f4142e18ad535d433
Shukrullojon <shukrullobk@gmail.com>
2024-03-16 06:50:05 +0500

    some code change

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Webhook;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
 class WebhookController extends Controller
 {
     public function index(Request $request){
-        Cache::forever('webhook-data',$request->all());
+        Webhook::create([
+            'request' => json_encode($request->all(),true),
+        ]);
         return true;
     }
 }
