49472573b3aca32629ff2f3c878b093dacbe2488
Shukrullojon <shukrullobk@gmail.com>
2024-03-18 12:26:55 +0500

    ok

---
+++ database/migrations/2024_03_18_070022_create_games_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('group_id');
+            $table->unsignedBigInteger('chat_id');
+            $table->tinyInteger('status')->default(0);
+            $table->unsignedBigInteger('circle_id')->nullable();
+            $table->bigInteger('month')->nullable();
+            $table->tinyInteger('childs')->nullable();
+            $table->unsignedBigInteger('money')->nullable();
+            $table->unsignedBigInteger('purpose_money')->nullable();
+            $table->tinyInteger('random')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
